76826a1b44282c836002fa2a5a6c666976f99c76
romain janssen <romain.janssen@viacesi.fr>
2017-10-22 18:52:12 +0200

    add eco file

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'getData',
+      script    : 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'boitier',
+      host : '10.0.2.15',
+      ref  : 'origin/master',
+      repo : 'https://github.com/AllienExia/boitier.git',
+      path : '/home/boitier/deploy/getData/',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
